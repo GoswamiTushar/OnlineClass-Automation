86b12746331a8f660ccad7153aa29cc386aca51d
Tushar Goswami <tushargoswami1510@gmail.com>
2020-11-24 21:55:07 +0530

    Modified script to hold both GLA portals credentials as well zoom credentials

--- credentials.py
+++ credentials.py
@@ -1,5 +1,17 @@
-def get_username():
-	return "username"
+class Credentials:
 
-def get_password():
-	return "password"
+	def get_GLA_UserID():
+		return ""
+
+	def get_GLA_Password():
+		return ""
+
+	def get_Zoom_UserID():
+		return ""
+
+	def get_Zoom_Password():
+		return ""
+
+if __name__ == '__main__':
+	print(Credentials.get_GLA_UserID(), Credentials.get_GLA_Password(), Credentials.get_Zoom_UserID(), Credentials.get_Zoom_Password(), sep = '\n')
+	
